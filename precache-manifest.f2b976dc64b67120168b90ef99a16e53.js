0e445d75ffd9831e22adac9743c60861df1f0f2e
Mauricio Cartagena <mc973220@gmail.com>
2022-04-29 08:48:21 -0400

    Updates

---
+++ precache-manifest.f2b976dc64b67120168b90ef99a16e53.js
@@ -0,0 +1,526 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b15aae3d97593229f4546707d75396",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b95803a6c2b399025ef",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "5f454a284e961ae387f7",
+    "url": "/static/css/main.88659253.chunk.css"
+  },
+  {
+    "revision": "3b95803a6c2b399025ef",
+    "url": "/static/js/2.22830276.chunk.js"
+  },
+  {
+    "revision": "9783afb4e0624be302f45b9bf81ea2ae",
+    "url": "/static/js/2.22830276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f454a284e961ae387f7",
+    "url": "/static/js/main.6a5837d0.chunk.js"
+  },
+  {
+    "revision": "89a68807ba62cc81e0de",
+    "url": "/static/js/runtime-main.2f27b35c.js"
+  },
+  {
+    "revision": "f670197f78819dc4f1f5ea3501632dcf",
+    "url": "/static/media/Logo.f670197f.png"
+  },
+  {
+    "revision": "79ea53fed59f391498dfc6f2fbea97c2",
+    "url": "/static/media/Merriweather-Bold.79ea53fe.ttf"
+  },
+  {
+    "revision": "fee74a810c9df73fe7218e4ff5815830",
+    "url": "/static/media/Merriweather-BoldItalic.fee74a81.ttf"
+  },
+  {
+    "revision": "b974ea37b30597dbf7b8d6d0ef38de89",
+    "url": "/static/media/Merriweather-Italic.b974ea37.ttf"
+  },
+  {
+    "revision": "e2f219e63a575a41e10f991e9c95819a",
+    "url": "/static/media/Merriweather-Regular.e2f219e6.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "1b38414956c666bd1df78fe5b9c84756",
+    "url": "/static/media/Montserrat-BoldItalic.1b384149.ttf"
+  },
+  {
+    "revision": "a7063e0c0f0cb546ad45e9e24b27bd3b",
+    "url": "/static/media/Montserrat-Italic.a7063e0c.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1d9c7945c7bc7dd0909105119bfbc191",
+    "url": "/static/media/OpenSans-Bold-webfont.1d9c7945.eot"
+  },
+  {
+    "revision": "2e90d5152ce92858b62ba053c7b9d2cb",
+    "url": "/static/media/OpenSans-Bold-webfont.2e90d515.woff"
+  },
+  {
+    "revision": "76cc6be5d8a231dc012fef4bdb86f79c",
+    "url": "/static/media/OpenSans-Bold-webfont.76cc6be5.ttf"
+  },
+  {
+    "revision": "93349923b5274a36ac93cb3168d09123",
+    "url": "/static/media/OpenSans-Bold-webfont.93349923.svg"
+  },
+  {
+    "revision": "2b4eeeaef53b3496a5cdf82803666ed7",
+    "url": "/static/media/OpenSans-BoldItalic-webfont.2b4eeeae.svg"
+  },
+  {
+    "revision": "6218c213bb8cf22b25710da6f3a90e48",
+    "url": "/static/media/OpenSans-BoldItalic-webfont.6218c213.eot"
+  },
+  {
+    "revision": "7657144ec477cd61ac4a5d1af3fa2d28",
+    "url": "/static/media/OpenSans-BoldItalic-webfont.7657144e.woff"
+  },
+  {
+    "revision": "b6690626036a7d6824632769305b1978",
+    "url": "/static/media/OpenSans-BoldItalic-webfont.b6690626.ttf"
+  },
+  {
+    "revision": "12be067a6270759b4f861d64cc267166",
+    "url": "/static/media/OpenSans-ExtraBold-webfont.12be067a.ttf"
+  },
+  {
+    "revision": "56d9d42e23863ce0ee8e8c2e2f22843f",
+    "url": "/static/media/OpenSans-ExtraBold-webfont.56d9d42e.eot"
+  },
+  {
+    "revision": "6ad396399f4022ccd1616cf11d0985be",
+    "url": "/static/media/OpenSans-ExtraBold-webfont.6ad39639.woff"
+  },
+  {
+    "revision": "ea3c4697897bd7f1505facb7084c6ac3",
+    "url": "/static/media/OpenSans-ExtraBold-webfont.ea3c4697.svg"
+  },
+  {
+    "revision": "042468300dab6f308592e15e02549c79",
+    "url": "/static/media/OpenSans-ExtraBoldItalic-webfont.04246830.woff"
+  },
+  {
+    "revision": "5517d73acdc17143c21b0cffab4cfaa9",
+    "url": "/static/media/OpenSans-ExtraBoldItalic-webfont.5517d73a.ttf"
+  },
+  {
+    "revision": "6512fca9ff4dc029307018c17e4a2a9d",
+    "url": "/static/media/OpenSans-ExtraBoldItalic-webfont.6512fca9.svg"
+  },
+  {
+    "revision": "ff3bedaecdec71c34b0ca85c3c495c2d",
+    "url": "/static/media/OpenSans-ExtraBoldItalic-webfont.ff3bedae.eot"
+  },
+  {
+    "revision": "43d5342998f3607bd61a8239e98b1160",
+    "url": "/static/media/OpenSans-Italic-webfont.43d53429.eot"
+  },
+  {
+    "revision": "5b774c25787e0a52c013463c9e3c4219",
+    "url": "/static/media/OpenSans-Italic-webfont.5b774c25.svg"
+  },
+  {
+    "revision": "de7ef31e6295902347c5c3643b2d82da",
+    "url": "/static/media/OpenSans-Italic-webfont.de7ef31e.ttf"
+  },
+  {
+    "revision": "f42641eed834f7b97a9499362c6c8855",
+    "url": "/static/media/OpenSans-Italic-webfont.f42641ee.woff"
+  },
+  {
+    "revision": "09e00aa7622ece30a0f1e06b55f66c2a",
+    "url": "/static/media/OpenSans-Light-webfont.09e00aa7.eot"
+  },
+  {
+    "revision": "2e98fc3ce85f31f63010b706259cb604",
+    "url": "/static/media/OpenSans-Light-webfont.2e98fc3c.ttf"
+  },
+  {
+    "revision": "45b47f3e9c7d74b80f5c6e0a3c513b23",
+    "url": "/static/media/OpenSans-Light-webfont.45b47f3e.woff"
+  },
+  {
+    "revision": "8f04ed9aeb2185499068d84842b95aa1",
+    "url": "/static/media/OpenSans-Light-webfont.8f04ed9a.svg"
+  },
+  {
+    "revision": "1d22953c479914c2f801e08de666b0e8",
+    "url": "/static/media/OpenSans-LightItalic-webfont.1d22953c.ttf"
+  },
+  {
+    "revision": "550b5fda4a27cfedb7131b1a6e85e748",
+    "url": "/static/media/OpenSans-LightItalic-webfont.550b5fda.eot"
+  },
+  {
+    "revision": "b553da506077488bc65289e10841d527",
+    "url": "/static/media/OpenSans-LightItalic-webfont.b553da50.woff"
+  },
+  {
+    "revision": "fd6dd5fa10c5a74f0a767eeb695342f1",
+    "url": "/static/media/OpenSans-LightItalic-webfont.fd6dd5fa.svg"
+  },
+  {
+    "revision": "488d5cc145299ba07b75495100419ee6",
+    "url": "/static/media/OpenSans-Regular-webfont.488d5cc1.ttf"
+  },
+  {
+    "revision": "79515ad0788973c533405f7012dfeccd",
+    "url": "/static/media/OpenSans-Regular-webfont.79515ad0.woff"
+  },
+  {
+    "revision": "8185eb3059c46e4169ce107dfcf85950",
+    "url": "/static/media/OpenSans-Regular-webfont.8185eb30.svg"
+  },
+  {
+    "revision": "c4d82460ef260eb1589e73528cbfb257",
+    "url": "/static/media/OpenSans-Regular-webfont.c4d82460.eot"
+  },
+  {
+    "revision": "3f6b1eed8a0832d6f316fc26526348a8",
+    "url": "/static/media/OpenSans-Semibold-webfont.3f6b1eed.svg"
+  },
+  {
+    "revision": "697574b47bcfdd2c45e3e63c7380dd67",
+    "url": "/static/media/OpenSans-Semibold-webfont.697574b4.woff"
+  },
+  {
+    "revision": "b32acea6fd3c228b5059042c7ad21c55",
+    "url": "/static/media/OpenSans-Semibold-webfont.b32acea6.ttf"
+  },
+  {
+    "revision": "f28eb362fb6afe946d822ee5451c2146",
+    "url": "/static/media/OpenSans-Semibold-webfont.f28eb362.eot"
+  },
+  {
+    "revision": "64f886b232962979e2eaf29d93108286",
+    "url": "/static/media/OpenSans-SemiboldItalic-webfont.64f886b2.ttf"
+  },
+  {
+    "revision": "70bafcaaadad9e17b9c7784abbc6b1c2",
+    "url": "/static/media/OpenSans-SemiboldItalic-webfont.70bafcaa.eot"
+  },
+  {
+    "revision": "70eb93d7ba2ad241180085a9a74b0b95",
+    "url": "/static/media/OpenSans-SemiboldItalic-webfont.70eb93d7.svg"
+  },
+  {
+    "revision": "719f7321a8366f4ee609737026432113",
+    "url": "/static/media/OpenSans-SemiboldItalic-webfont.719f7321.woff"
+  },
+  {
+    "revision": "9fd9b872c5d77a8dbe3d54c55b359e79",
+    "url": "/static/media/acsyos.9fd9b872.png"
+  },
+  {
+    "revision": "512d9401f20c70712fd3483e98ee582a",
+    "url": "/static/media/basicojavase.512d9401.webp"
+  },
+  {
+    "revision": "643786494b3a954c5b26164dc6bc36df",
+    "url": "/static/media/calendar-app.64378649.png"
+  },
+  {
+    "revision": "3775887904248f40da1469cd27d91791",
+    "url": "/static/media/computacion-basica.37758879.webp"
+  },
+  {
+    "revision": "e0283aabb0f42ea588b635fed18f64df",
+    "url": "/static/media/cosmos.e0283aab.png"
+  },
+  {
+    "revision": "5fc46c11a327645616ce6352586173e6",
+    "url": "/static/media/desarrollo-habitos-crecimiento-personal-5739b7cc-6340-439a-aa4a-f9bb3a32058d.5fc46c11.webp"
+  },
+  {
+    "revision": "662a1d9c17a177c62c9320bc08c23d9b",
+    "url": "/static/media/desing-para-programadores.662a1d9c.webp"
+  },
+  {
+    "revision": "0b4be124c2f40c9f838b83a3cb36588f",
+    "url": "/static/media/diploma-computacion-basica.0b4be124.pdf"
+  },
+  {
+    "revision": "ceea5c35d160da5d0a1312a05ae3ae35",
+    "url": "/static/media/diploma-creacion-habitos.ceea5c35.pdf"
+  },
+  {
+    "revision": "324c45233f119448f2a7e15204cd51c2",
+    "url": "/static/media/diploma-desing-programadores.324c4523.pdf"
+  },
+  {
+    "revision": "34b1e0c80a1b4473a91070dd256cf193",
+    "url": "/static/media/diploma-estrategias-ingles.34b1e0c8.pdf"
+  },
+  {
+    "revision": "14657467a80395bf5a5d69946edbefc4",
+    "url": "/static/media/diploma-gestion-tiempo.14657467.pdf"
+  },
+  {
+    "revision": "b60389d0c302ea0ffeb8245d19aa3a74",
+    "url": "/static/media/diploma-hablar-en-publico.b60389d0.pdf"
+  },
+  {
+    "revision": "ad4deb6e284cd10184402225506be1f4",
+    "url": "/static/media/diploma-ingles-principiantes.ad4deb6e.pdf"
+  },
+  {
+    "revision": "0c4f1a809b9e37dea6ee3a7fbb132d6f",
+    "url": "/static/media/diploma-java-basico.0c4f1a80.pdf"
+  },
+  {
+    "revision": "4de4064f4da868690e4156e4e57f964a",
+    "url": "/static/media/diploma-java-oop.4de4064f.pdf"
+  },
+  {
+    "revision": "3dabdf4e3c6eace5bed2475a1b84804c",
+    "url": "/static/media/diploma-marca-personal.3dabdf4e.pdf"
+  },
+  {
+    "revision": "9584dbe328af6cfb7e0d06a40836d098",
+    "url": "/static/media/diploma-portafolios.9584dbe3.pdf"
+  },
+  {
+    "revision": "260808455fda1b8d179c759eefd9c458",
+    "url": "/static/media/diploma-postgresql.26080845.pdf"
+  },
+  {
+    "revision": "40488d026ecf6196d11a43c24b942e8f",
+    "url": "/static/media/diploma-prework-linux.40488d02.pdf"
+  },
+  {
+    "revision": "8070bf3b64e2803394b905a1367480c6",
+    "url": "/static/media/diploma-prework-macos.8070bf3b.pdf"
+  },
+  {
+    "revision": "b5c0485d11f7ee0e2c9dee36432b009a",
+    "url": "/static/media/diploma-prework-windows.b5c0485d.pdf"
+  },
+  {
+    "revision": "2d91b6fb448bcb171d59e8ef5ca58da8",
+    "url": "/static/media/diploma-productividad.2d91b6fb.pdf"
+  },
+  {
+    "revision": "6b0415f71b05cbd602a34ba667a932f8",
+    "url": "/static/media/diploma-react-avanzado.6b0415f7.pdf"
+  },
+  {
+    "revision": "3d2e4d88bd3d3627ee48c3284722b748",
+    "url": "/static/media/diploma-react-router-redux.3d2e4d88.pdf"
+  },
+  {
+    "revision": "31b671ad332c87bd0b92d52d02acf74c",
+    "url": "/static/media/en.31b671ad.webp"
+  },
+  {
+    "revision": "90ee1d3978b2be42cfdb8f08a9579871",
+    "url": "/static/media/flutter.90ee1d39.png"
+  },
+  {
+    "revision": "0a799148a50bb02c6f380eabd8d97559",
+    "url": "/static/media/fontawesome-webfont.0a799148.svg"
+  },
+  {
+    "revision": "3293616ec0c605c7c2db25829a0a509e",
+    "url": "/static/media/fontawesome-webfont.3293616e.woff"
+  },
+  {
+    "revision": "8b27bc96115c2d24350f0d09e6a9433f",
+    "url": "/static/media/fontawesome-webfont.8b27bc96.eot"
+  },
+  {
+    "revision": "dcb26c7239d850266941e80370e207c1",
+    "url": "/static/media/fontawesome-webfont.dcb26c72.ttf"
+  },
+  {
+    "revision": "3cd7423df7d808f922898f5be0edcdd8",
+    "url": "/static/media/fontello.3cd7423d.ttf"
+  },
+  {
+    "revision": "931bc5e57d402b88137500d8af868cff",
+    "url": "/static/media/fontello.931bc5e5.svg"
+  },
+  {
+    "revision": "b54500d9503d30109f1a6acdb9fe81d8",
+    "url": "/static/media/fontello.b54500d9.woff"
+  },
+  {
+    "revision": "ce25cd5bdc97f3162b5ed8d9facb3005",
+    "url": "/static/media/fontello.ce25cd5b.eot"
+  },
+  {
+    "revision": "31a872359e87499d26d4b7893d81d2d0",
+    "url": "/static/media/fpotify.31a87235.png"
+  },
+  {
+    "revision": "3f4961d3d053030895e296bf4353997f",
+    "url": "/static/media/gestion-tiempo.3f4961d3.webp"
+  },
+  {
+    "revision": "f03f9795067d3dd6088b3a2095c61c24",
+    "url": "/static/media/git-github.f03f9795.png"
+  },
+  {
+    "revision": "b94f6aa47876f850b8c160735230f9fe",
+    "url": "/static/media/hablar-publico.b94f6aa4.webp"
+  },
+  {
+    "revision": "f7b724b19417414e685e48cc676482c7",
+    "url": "/static/media/herramientas.f7b724b1.png"
+  },
+  {
+    "revision": "a8c4cfcf6aefc7db3f4ac8ddc1f0c952",
+    "url": "/static/media/ingles-para-principiantes.a8c4cfcf.webp"
+  },
+  {
+    "revision": "95a25e5353b923c491951407bf9a9248",
+    "url": "/static/media/javascript.95a25e53.png"
+  },
+  {
+    "revision": "6b38d2ab8afc1a35432a234023151cee",
+    "url": "/static/media/journal.6b38d2ab.png"
+  },
+  {
+    "revision": "c6e0e63ec9bb863594e6651023a1b196",
+    "url": "/static/media/jquery-avanzado.c6e0e63e.png"
+  },
+  {
+    "revision": "c6e0e63ec9bb863594e6651023a1b196",
+    "url": "/static/media/jquery.c6e0e63e.png"
+  },
+  {
+    "revision": "2917521479ef16dd499d2f0c4bdd2984",
+    "url": "/static/media/js-moderno.29175214.png"
+  },
+  {
+    "revision": "6be4bfc9a081cb82910a1ac43eaee4c7",
+    "url": "/static/media/librebaskerville-bold-webfont.6be4bfc9.svg"
+  },
+  {
+    "revision": "c49b3981651dfe7f281418762ba6ead9",
+    "url": "/static/media/librebaskerville-bold-webfont.c49b3981.eot"
+  },
+  {
+    "revision": "d1e41b59c942f200e2af5cd00a1c413c",
+    "url": "/static/media/librebaskerville-bold-webfont.d1e41b59.woff"
+  },
+  {
+    "revision": "e2a49a303079f1d0ef576591d806b1b4",
+    "url": "/static/media/librebaskerville-bold-webfont.e2a49a30.ttf"
+  },
+  {
+    "revision": "8825a887e1d72aad739281fcc3eb83a6",
+    "url": "/static/media/librebaskerville-italic-webfont.8825a887.svg"
+  },
+  {
+    "revision": "98770575e43bc5883a62f232c2fc5475",
+    "url": "/static/media/librebaskerville-italic-webfont.98770575.eot"
+  },
+  {
+    "revision": "bfa5b7beba080b0c6af53ca5a255b37d",
+    "url": "/static/media/librebaskerville-italic-webfont.bfa5b7be.woff"
+  },
+  {
+    "revision": "e145c25e7611072d1d1991eb12d61bda",
+    "url": "/static/media/librebaskerville-italic-webfont.e145c25e.ttf"
+  },
+  {
+    "revision": "0417aa96d0b9b04ba4b3a5c3a2378ea7",
+    "url": "/static/media/librebaskerville-regular-webfont.0417aa96.svg"
+  },
+  {
+    "revision": "84aa9cfe8f8c7a9a0ed814c1d54e6f87",
+    "url": "/static/media/librebaskerville-regular-webfont.84aa9cfe.eot"
+  },
+  {
+    "revision": "d67a803b3388b5b73200e6eec553f6fe",
+    "url": "/static/media/librebaskerville-regular-webfont.d67a803b.woff"
+  },
+  {
+    "revision": "eaf077e2e9d8c00e3348b03a7cac92fe",
+    "url": "/static/media/librebaskerville-regular-webfont.eaf077e2.ttf"
+  },
+  {
+    "revision": "00e12f6dd9437b6797828f54279c98a1",
+    "url": "/static/media/marca-personal.00e12f6d.webp"
+  },
+  {
+    "revision": "cd9398cbcbb49abb12c8116b028a9ae4",
+    "url": "/static/media/node-cero-experto.cd9398cb.png"
+  },
+  {
+    "revision": "37b2e22ac42fc65633c180509b4b380f",
+    "url": "/static/media/photo.37b2e22a.jpeg"
+  },
+  {
+    "revision": "506b320fdee514747ad520e5f9c5c3a4",
+    "url": "/static/media/poo-java-se.506b320f.webp"
+  },
+  {
+    "revision": "323cbb009677b096003c9eb1dc89bf52",
+    "url": "/static/media/portafolio-cv.323cbb00.webp"
+  },
+  {
+    "revision": "e20ff1b159da6da35760de861f5ecd89",
+    "url": "/static/media/postgresql.e20ff1b1.webp"
+  },
+  {
+    "revision": "dc0bb6494ccd3e4dae08427bf162bad7",
+    "url": "/static/media/prework-configuracion-entorno-mac-.dc0bb649.webp"
+  },
+  {
+    "revision": "76ae8c38ddc67115a01c89186d24e2c5",
+    "url": "/static/media/prework-configuracion-entorno-windows.76ae8c38.webp"
+  },
+  {
+    "revision": "a1fb4071b52fa3e60b7bb56ada22d129",
+    "url": "/static/media/prework-linux.a1fb4071.webp"
+  },
+  {
+    "revision": "39ff458b7eb84378194117d73cc57689",
+    "url": "/static/media/productividad.39ff458b.webp"
+  },
+  {
+    "revision": "9df822a5936306bc05559230df410431",
+    "url": "/static/media/progra-101.9df822a5.png"
+  },
+  {
+    "revision": "587d41da6f31a9883308f0edc73a4a47",
+    "url": "/static/media/react-redux.587d41da.webp"
+  },
+  {
+    "revision": "c545ff263fc8b92e1fc02792a9440560",
+    "url": "/static/media/react.c545ff26.png"
+  },
+  {
+    "revision": "48fb8cd9b15605e60a23d0d9560c4ab8",
+    "url": "/static/media/reactjs-avanzado.48fb8cd9.webp"
+  },
+  {
+    "revision": "8acff98d4a3493eb1e00bb770a4ef491",
+    "url": "/static/media/restful.8acff98d.png"
+  },
+  {
+    "revision": "51334736afc0dc0ab23b8b4592a334cc",
+    "url": "/static/media/steven.51334736.png"
+  },
+  {
+    "revision": "0473993502a93331619977f827ea3117",
+    "url": "/static/media/vscode.04739935.png"
+  }
+]);
